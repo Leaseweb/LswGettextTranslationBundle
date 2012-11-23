c68ad383628f1beede3883ce9b2bbe10a01b90b2
Maurits van der Schee <m.vanderschee@leaseweb.com>
2012-11-23 09:53:40 +0100

    removed copied over comments

--- Command/ExtractApplicationCommand.php
+++ Command/ExtractApplicationCommand.php
@@ -1,14 +1,5 @@
 <?php
 
-/*
- * This file is part of the FOSUserBundle package.
- *
- * (c) FriendsOfSymfony <http://friendsofsymfony.github.com/>
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
-
 namespace Lsw\GettextTranslationBundle\Command;
 
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
