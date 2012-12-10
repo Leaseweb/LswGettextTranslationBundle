e28868afa0f450c1b9b0d5c77da5ec53086339da
Evgeniy Sokolov <ewgraf@gmail.com>
2012-12-10 11:55:01 +0600

    more friendly code for kernel request listener

--- Routing/GettextLocaleListener.php
+++ Routing/GettextLocaleListener.php
@@ -26,44 +26,68 @@ class GettextLocaleListener
   
   public function onKernelRequest(GetResponseEvent $event)
   {
-    if (HttpKernelInterface::MASTER_REQUEST === $event->getRequestType())
-    {
-      $context = $this->router->getContext();
-      $current = setlocale(LC_MESSAGES, 0);
-      if ($current && strpos($current,'.'))
-      {
-        list($current,$charset) = explode('.',$current);
-      }
-      /*$requested = isset($_SESSION['symfony/user/sfUser/culture'])
-		    ? $_SESSION['symfony/user/sfUser/culture']
-                    : $context->getParameter('_locale');*/
-      $requested = $context->getParameter('_locale');
-      if ($requested && strlen($requested)<5 && isset($this->localeShortcuts[$requested]))
-      {
-        $requested=$this->localeShortcuts[$requested];
-      }
-      if ($requested && $current != $requested)
-      {
-        $request = $event->getRequest();
-        # ugly backwards compatibility fix for symfony 2.0 (start)
-        if (!method_exists($request, 'setLocale')) $request = $request->getSession();
-        # ugly backwards compatibility fix for symfony 2.0 (end)
-        if (!setlocale(LC_MESSAGES, $requested.'.UTF-8', $requested.'.utf8', $requested.'.utf-8', $requested.'UTF8'))
-        { 
-          $request->setLocale(null);
-          throw new InvalidParameterException("Requested locale '$requested' could not be set. Is this locale installed? Hint: Execute 'locale -a' on the Linux command line to list installed locales.");
-        }
-        $request->setLocale($requested);
-      }
+    if (HttpKernelInterface::MASTER_REQUEST !== $event->getRequestType())
+      return;
 
-      $version = file_exists($this->rootDir.'/Resources/gettext/version')
-                   ? file_get_contents($this->rootDir.'/Resources/gettext/version')
-                   : "";
-      
-      // bind the default domain to the combined translations
-      bindtextdomain('messages' . $version, $this->rootDir . '/Resources/gettext/combined/');
-      textdomain('messages' . $version);
-      bind_textdomain_codeset('messages' . $version, 'UTF-8');
+    $currentLocale = $this->getCurrentLocale();
+    $requestedLocale = $this->getRequestedLocale();
+
+    if ($requestedLocale && $currentLocale != $requestedLocale)
+    {
+      $request = $event->getRequest();
+      # ugly backwards compatibility fix for symfony 2.0 (start)
+      if (!method_exists($request, 'setLocale')) $request = $request->getSession();
+      # ugly backwards compatibility fix for symfony 2.0 (end)
+      if (!setlocale(LC_MESSAGES, $requestedLocale.'.UTF-8', $requestedLocale.'.utf8', $requestedLocale.'.utf-8', $requestedLocale.'UTF8'))
+      {
+        $request->setLocale(null);
+        throw new InvalidParameterException("Requested locale '$requestedLocale' could not be set. Is this locale installed? Hint: Execute 'locale -a' on the Linux command line to list installed locales.");
+      }
+      $request->setLocale($requestedLocale);
     }
+
+    $this->setupGetText();
+  }
+
+  private function getCurrentLocale()
+  {
+  	$current = setlocale(LC_MESSAGES, 0);
+  	if ($current && strpos($current,'.'))
+  	{
+  		list($current,$charset) = explode('.',$current);
+  	}
+
+  	return $current;
+  }
+
+  private function getRequestedLocale()
+  {
+  	$context = $this->router->getContext();
+
+  	/*$requested = isset($_SESSION['symfony/user/sfUser/culture'])
+  	 ? $_SESSION['symfony/user/sfUser/culture']
+  	: $context->getParameter('_locale');*/
+  	$requested = $context->getParameter('_locale');
+  	if ($requested && strlen($requested)<5 && isset($this->localeShortcuts[$requested]))
+  	{
+  		$requested=$this->localeShortcuts[$requested];
+  	}
+
+  	return $requested;
+  }
+
+  private function setupGetText()
+  {
+  	$versionFile = $this->rootDir.'/Resources/gettext/version';
+
+  	$version = file_exists($versionFile)
+              	? file_get_contents($versionFile)
+  	            : '';
+
+  	$domain = 'messages' . $version;
+
+  	bindtextdomain($domain, $this->rootDir . '/Resources/gettext/combined/');
+  	textdomain($domain);
+  	bind_textdomain_codeset($domain, 'UTF-8');
   }
 }
